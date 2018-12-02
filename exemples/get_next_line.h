85c7d3487e424b553dff87a0b7c29ee4805bf682
GHANIME <ghanime.charles@gmail.com>
2018-12-03 00:04:17 +0100

    maj gnl

---
+++ exemples/get_next_line.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cghanime <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/29 19:04:42 by cghanime          #+#    #+#             */
+/*   Updated: 2018/12/02 04:50:36 by cghanime         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+#define GET_NEXT_LINE_H
+
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+# include "libft.h"
+
+# define BUFF_SIZE 10
+
+int		get_next_line(const int fd, char **line);
+
+#endif
--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: cghanime <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/28 17:41:11 by cghanime          #+#    #+#             */
-/*   Updated: 2018/11/30 18:55:25 by cghanime         ###   ########.fr       */
+/*   Updated: 2018/12/02 16:48:52 by cghanime         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ int		get_next_line(const int fd, char **line)
 			rest = ft_strdup(buffer);
 		else
 		{
-			tmp = malloc(sizeof(char ) * ft_strlen(rest) + BUFFER_SIZE + 1);
+			tmp = malloc(sizeof(char ) * ft_strlen(rest) + ft_strlen(buffer) + 1);
 			ft_strcpy(tmp, rest);
 			ft_strcat(tmp, buffer);
 			rest = tmp;
