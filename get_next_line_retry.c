7eab96f01fa61a771e27fcdf55d2df768f7a17e7
GHANIME <ghanime.charles@gmail.com>
2018-12-19 01:59:50 +0100

    retry gnl

--- get_next_line_retry.c
+++ get_next_line_retry.c
@@ -6,41 +6,56 @@
 /*   By: cghanime <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/16 18:15:01 by cghanime          #+#    #+#             */
-/*   Updated: 2018/12/18 01:45:07 by cghanime         ###   ########.fr       */
+/*   Updated: 2018/12/19 01:17:51 by cghanime         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "get_next_line.h"
 # include "libft/libft.h"
 
+
+
 int		get_next_line(const int fd, char **line)
 {
+	char		buff[BUFF_SIZE + 1];
 	int		ret;
-	char	buffer[BUFF_SIZE + 1];
 	t_list	*lst;
-	char	*tmp;
-	int		endl;
 
-	if (!line || fd < 0 || BUFF_SIZE <= 0)
-		return (-1);
-	while ((ret = read(fd, buffer, BUFF_SIZE)) > 0 && !ft_strchr(buffer, '\n'))
-	{
-		buffer[ret] = '\0';
-		tmp = lst->content;
-		lst->content = ft_strjoin(lst->content, buffer);
-		ft_strdel(&tmp);
-	}
-	if (ret == -1 || !*(tmp = lst->content))
-		return (ret = -1 ? -1 : 0);
-	if ((endl = (ft_strchr(lst->content, '\n') > 0)))
-		*line = ft_strsub(lst->content, 0, ft_strchr(lst->content, '\n')
-		- (char *)(lst->content));
-	*line = ft_strdup(lst->content);
-	lst->content = ft_strsub(lst->content, (unsigned int)(ft_strlen(*line)
-	+ endl), (size_t)(ft_strlen(lst->content) - (ft_strlen(*line) + endl)));
-	ft_strdel(&tmp);
-	return (!(!lst->content && !ft_strlen(*line)));
+	while ((ret = read(fd, buff, BUFF_SIZE) > 0) && 
+	!(ft_strchr(buff, '\n')) && !(ft_strchr(buff, '\0')))
+		{
+			if (lst->content)
+			{
+				
+			}
+			buff[ret] = '\0';
+			ft_memccpy(*line, buff, '\n', ret);
+			if (BUFF_SIZE - ret != 0)
+				{
+					ft_strsub(lst->content, ft_strlen(*line) + 1,
+					ft_strlen(lst->content) - ft_strlen(*line));
+				}
+		}
+	return (**line);
 }
+
+/*
+void	ft_multi_fd(int fd, t_list *lst)
+{
+	if (lst)
+	{
+		while (lst)
+		{
+			if (lst->content_size = fd)
+				return (fd)
+			lst = ft_lstnew(NULL, 0);
+			lst->content_size = fd;
+		}
+	}
+	lst = ft_lstnew(NULL, 0);
+	lst->content_size = fd;
+}
+*/
 
 /*
  ** MAIN TEST
@@ -61,7 +76,11 @@ int		main(int argc, char **argv)
 			return (1);
 		}
 
-		get_next_line(fd, &tab);
+		while (get_next_line(fd, &tab) > 0)
+		{
+			printf("%s\n", tab);
+			free(&tab);
+		}
 
 		if (close(fd) == -1)
 		{
